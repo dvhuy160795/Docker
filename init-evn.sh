c12d0f9e9bec17490230531cf53976eb179ebcbf
dvhuy160795 <dvhuy160795@gmail.com>
2020-01-26 21:15:20 +0700

    add file evn setup

--- init-evn.sh
+++ init-evn.sh
@@ -1,9 +1,9 @@
-sudo apt-get install php
+sudo apt-get install php php-cli php-zip wget unzip
 
-sudo yum install php
+sudo yum install php php-cli php-zip wget unzip
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-mv composer.phar /usr/local/bin/composer
+composer
