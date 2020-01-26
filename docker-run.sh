469f5ee2b5ff776ea4ddf5af388d090be8d8a07b
dvhuy160795 <dvhuy160795@gmail.com>
2020-01-26 18:48:49 +0700

    fix docker run

--- docker-run.sh
+++ docker-run.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCRIPT=`readlink -f $0`
-DIR=`dirname $SCRIPT`
-PROJECT_DIR="$(dirname $(dirname $DIR))"
-DOCKER_FILES="-f docker-compose.yml"
-
-cd $PROJECT_DIR/Docker
-# Create global network
-docker network inspect my-network &>/dev/null || docker network create --driver bridge my-network
-
-#run docker-compose
-docker-compose up
-
 cd Docker
 composer install
+
+#run docker-compose
+cd ..
+docker-compose up
