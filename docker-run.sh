61f65d3237d4b400701680b0045d92fed4d052ef
dvhuy160795 <dvhuy160795@gmail.com>
2020-01-26 21:20:05 +0700

    add file evn setup

--- docker-run.sh
+++ docker-run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 composer create-project --prefer-dist laravel/laravel Docker
 
+php /usr/local/bin/composer create-project --prefer-dist laravel/laravel Docker
+
 cd Docker
 chmod -R 777 storage/
 composer install
 
+php /usr/local/bin/composer install
 #run docker-compose
 cd ..
 docker-compose up
