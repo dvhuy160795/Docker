56212af7aca3d973a4da244bcce167d90aee0181
dvhuy160795 <dvhuy160795@gmail.com>
2020-01-27 18:12:58 +0700

    fix develop

--- setupEvn/ubuntu/docker-run-ubuntu.sh
+++ setupEvn/ubuntu/docker-run-ubuntu.sh
@@ -3,13 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SCRIPT=$(readlink -f $0)
 SOURCEPATH=`dirname $SCRIPT`
 
-composer create-project --prefer-dist laravel/laravel $SOURCEPATH/../../Docker
-
-
-chmod -R 777 $SOURCEPATH/../../Docker/storage/
-(cd $SOURCEPATH/../../Docker && exec composer install)
-(cd $SOURCEPATH/../../Docker && exec chmod -R 777 storage/)
-
 #run docker-compose
 docker rm -f huydv-mysql huydv-httpd huydv-php huydv-memcached
 docker-compose -f $SOURCEPATH/../docker-compose.yml up
